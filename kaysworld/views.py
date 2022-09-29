50519292d5c4a46a3c49f8aa2f888dd66204cd69
Kaylin Haill <kaylin.haill@hotmail.com>
2022-09-29 18:58:48 -0400

    Deploy to Heroku

--- kaysworld/views.py
+++ kaysworld/views.py
@@ -1,6 +1,14 @@
 # mysite/views.py
+"""
+A simple function-based view
+"""
 
 from django.http import HttpResponse
 
 def index(request):
+    """
+    This view generates a simple HTTP response using the
+    HttpResponse constructor imported from the http library
+    of the Django framework.
+    """
     return HttpResponse('Hello world!')
--- tests/kaysworld/test_views.py
+++ tests/kaysworld/test_views.py
@@ -1,6 +1,14 @@
 # test_views.py
+"""
+Test views for kaysworld
+"""
 
 def test_index_ok(client):
+    """
+    A simple test which ensures that a request made to
+    the site's root index (i.e. '/') responds with an HTTP "OK"
+    response, which is represented by the status code 200.
+    """
     # Make a GET request to / and store the response object
     # using the Django test client.
     response = client.get('/')
